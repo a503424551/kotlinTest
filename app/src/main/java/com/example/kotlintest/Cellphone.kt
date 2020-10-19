a7ad0ecbf0436f6f33ae20e245b1dd23ce7808cb
a503424551 <503424551@qq.com>
2020-10-19 10:51:19 +0800

    集合类2

--- app/src/main/java/com/example/kotlintest/Cellphone.kt
+++ app/src/main/java/com/example/kotlintest/Cellphone.kt
@@ -4,8 +4,8 @@ fun main() {
     val s1 = singleObj
     val s2 = singleObj
     //print(s1.equals(s2))
-    s2.listTest()
-    // s2.mapTest()
+      s2.listTest()
+    //  s2.mapTest()
     /* val  c=Cellphone("xiaomi","02415")
     val c2=Cellphone("xiaomi","02415")
         print(c==c2)*/
@@ -21,15 +21,26 @@ object singleObj {
     fun listTest() {
         //  val list = listOf("ssa", "sds", 1)
         val list = mutableListOf("sa","ds","sasadddd")
-        println(list.maxBy { it.length})
-
+       // println(list.maxBy { it.length})
+    val  anyRes=list.any { it.length>5 }
+        val allRes=list.all { it.length>3 }
+        print(anyRes.toString()+"--"+allRes)
        /* for (l in list) {
             print(l)
         }*/
     }
 
     fun mapTest(){
-        val  mapTest= mutableMapOf("wa" to 0,1 to 2)
+  //      val  mapTest= mutableMapOf("wa" to 0,1 to 2)
+        val  mapTest= mutableListOf("aaa","ssss", "ddsdsds", "fdf")
+
+
+      //  val  newMap= mapTest.map { it.toUpperCase() }
+        val newMap=mapTest.filter { it.length>3 }.map{it.toUpperCase()}
+        for (f in newMap){
+            print(f)
+        }
+
       /* for (ent in mapTest){
             print(ent.key.toString()+"-"+ent.value)
         }*/
