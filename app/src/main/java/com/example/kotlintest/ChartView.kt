0b4668563cf67e653395eef0f9c0fdd008d72f76
a503424551 <503424551@qq.com>
2020-10-20 13:39:37 +0800

    kotlin   Y轴

--- app/src/main/java/com/example/kotlintest/ChartView.kt
+++ app/src/main/java/com/example/kotlintest/ChartView.kt
@@ -3,19 +3,35 @@ package com.example.kotlintest
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.DashPathEffect
 import android.graphics.Paint
 import android.view.View
 
+
 class ChartView(context: Context?) : View(context) {
+    var  ymaxValue=100f
+    var  yValue=0f
+    var yLinesHeight=0f
+    var yLinesNumber=5//Y轴刻度线条数
     var  paintXY=Paint()
     val marg = 50f//整个view 距离父控件距离
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         setBackgroundColor(Color.GRAY)
         paintXY.setColor(Color.WHITE)
-        paintXY.strokeWidth=2f
+        paintXY.strokeWidth=6f
 
-        canvas?.drawLine(marg, marg, marg,height- marg,paintXY)//画Y轴
-        canvas?.drawLine(marg,height-marg,width-marg,height-marg,paintXY)//画X轴
+        canvas?.drawLine(marg, marg, marg, height - marg, paintXY)//画Y轴
+        canvas?.drawLine(marg, height - marg, width - marg, height - marg, paintXY)//画X轴
+
+        paintXY.strokeWidth=1f
+        yLinesHeight=(height-marg*2)/(yLinesNumber)
+        yValue=ymaxValue/5
+        for (i in 0 until yLinesNumber){
+            canvas?.drawLine(marg, marg + yLinesHeight * i, width - marg, marg + yLinesHeight * i, paintXY)
+            canvas?.drawText((ymaxValue-yValue*i).toString(),10f,(marg + yLinesHeight * i)+5f,paintXY)
+        }
+
+
     }
 }
