ed743ecce72cbc1a85c842381b2790ad0db47709
a503424551 <503424551@qq.com>
2020-10-21 15:38:35 +0800

    优化两种使用方式

--- app/src/main/java/com/example/kotlintest/ChartView.kt
+++ app/src/main/java/com/example/kotlintest/ChartView.kt
@@ -4,10 +4,21 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.util.AttributeSet
 import android.view.View
 
 
-class ChartView(context: Context?) : View(context) {
+class ChartView : View {
+
+    constructor(mContext: Context) : super(mContext) {
+
+    }
+
+    constructor(mContext: Context, mAttributeSet: AttributeSet) : super(mContext, mAttributeSet) {
+
+    }
+
+
 
     var charWidth=50f//柱状的宽度
     var chartsMarg=30f//柱状间的间隔距离
@@ -32,34 +43,68 @@ class ChartView(context: Context?) : View(context) {
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         setBackgroundColor(Color.GRAY)
+        if(list.size>0){
+            notifyView(canvas)
+        }
+
+
+
+    }
+
+    private fun notifyView(canvas: Canvas?) {
         paintXY.setColor(Color.WHITE)
-        paintXY.strokeWidth=6f
+        paintXY.strokeWidth = 6f
 
         canvas?.drawLine(marg, marg, marg, height - marg, paintXY)//画Y轴
         canvas?.drawLine(marg, height - marg, width - marg, height - marg, paintXY)//画X轴
 
-        paintXY.strokeWidth=1f
-        yLinesHeight=(height-marg*2)/(yLinesNumber)
-        yValue=ymaxValue/yLinesNumber
-        for (i in 0 until yLinesNumber){
-            canvas?.drawLine(marg, marg + yLinesHeight * i, width - marg, marg + yLinesHeight * i, paintXY)
-            canvas?.drawText((ymaxValue-yValue*i).toInt().toString(),10f,(marg + yLinesHeight * i)+5f,paintXY)
+        paintXY.strokeWidth = 1f
+        yLinesHeight = (height - marg * 2) / (yLinesNumber)
+        yValue = ymaxValue / yLinesNumber
+        for (i in 0 until yLinesNumber) {
+            canvas?.drawLine(
+                marg,
+                marg + yLinesHeight * i,
+                width - marg,
+                marg + yLinesHeight * i,
+                paintXY
+            )
+            canvas?.drawText(
+                (ymaxValue - yValue * i).toInt().toString(),
+                10f,
+                (marg + yLinesHeight * i) + 5f,
+                paintXY
+            )
         }
 
-     //   for (i in 1 until chartCount+1){
-        for(c in list){
-       var i=list.indexOf(c)+1
+        //   for (i in 1 until chartCount+1){
+        for (c in list) {
+            var i = list.indexOf(c) + 1
 
-        var charRealHeigthValue= c.xValue
+            var charRealHeigthValue = c.xValue
 
-        val fl = (height - marg * 2) * (charRealHeigthValue / ymaxValue)
-        var charRealHeigth=(height-marg*2)- fl +marg
-             canvas?.drawRect(marg+chartsMarg*i+charWidth*(i-1),charRealHeigth,marg+chartsMarg*i+charWidth*i,height-marg,paintXY)
-            canvas?.drawText(c.xName,marg+chartsMarg*i+charWidth*(i-1)+15,height-marg+20,paintXY)
-            canvas?.drawText(charRealHeigthValue.toString(),marg+chartsMarg*i+charWidth*(i-1)+15,charRealHeigth-5,paintXY)
+            val fl = (height - marg * 2) * (charRealHeigthValue / ymaxValue)
+            var charRealHeigth = (height - marg * 2) - fl + marg
+            canvas?.drawRect(
+                marg + chartsMarg * i + charWidth * (i - 1),
+                charRealHeigth,
+                marg + chartsMarg * i + charWidth * i,
+                height - marg,
+                paintXY
+            )
+            canvas?.drawText(
+                c.xName,
+                marg + chartsMarg * i + charWidth * (i - 1) + 15,
+                height - marg + 20,
+                paintXY
+            )
+            canvas?.drawText(
+                charRealHeigthValue.toString(),
+                marg + chartsMarg * i + charWidth * (i - 1) + 15,
+                charRealHeigth - 5,
+                paintXY
+            )
         }
-
-
     }
 }
 class chartBean (var  displayXValue :String,var xValue:Int,var xName:String){
--- app/src/main/java/com/example/kotlintest/MainActivity.kt
+++ app/src/main/java/com/example/kotlintest/MainActivity.kt
@@ -6,15 +6,27 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val chartView= ChartView(this);
-        var list= mutableListOf<chartBean>()
-        var max=600
-        for ( i in 0..10){
-           var chartView=chartBean("",(0..max).random(),"小米")
+      val chartView = ChartView(this)
+        setChartView(chartView)
+        setContentView(chartView)
+
+
+    /*    setContentView(R.layout.activity_main)
+        val chartView = findViewById<ChartView>(R.id.cv)
+        setChartView(chartView)*/
+
+    }
+
+    private fun setChartView(chartView :ChartView){
+        val chartView = chartView;
+        var list = mutableListOf<chartBean>()
+        var max = 600
+        for (i in 0..10) {
+            var chartView = chartBean("", (0..max).random(), "小米")
             list.add(chartView)
         }
         chartView.setChatBeans(list)
-        chartView.setYData(7,max.toFloat())
-        setContentView(chartView)
+        chartView.setYData(6, max.toFloat())
+        chartView.postInvalidate()
     }
 }
